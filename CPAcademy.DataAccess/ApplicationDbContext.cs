4aea1473304a527eb4145937ac053b2b503bede2
Mohamed Hazem <100438594+MohamedHazem5@users.noreply.github.com>
2023-08-10 00:46:49 +0300

    Add project files.

---
+++ CPAcademy.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CPAcademy.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
