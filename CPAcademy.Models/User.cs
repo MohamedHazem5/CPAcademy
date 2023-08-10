2049f66bdca0d023c1419ccc9d6298ae254cba2c
Moamen Mostafa <mmoamen270@gmail.com>
2023-08-10 16:28:01 +0300

    Edit User Model

--- CPAcademy.Models/User.cs
+++ CPAcademy.Models/User.cs
@@ -8,8 +8,6 @@ namespace CPAcademy.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
         public string PhoneNumber { get; set; }
         public bool Gender { get; set; }
         public int EXP { get; set; }
