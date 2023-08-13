383eb8eddbb830886447cd3889c50435a45007fe
Mohamed Hazem <100438594+MohamedHazem5@users.noreply.github.com>
2023-08-13 19:26:06 +0300

    Added BaseAPIController

---
+++ CPAcademy/Controllers/BaseAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CPAcademy.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class BaseAPIController : ControllerBase
+    {
+    }
+}
