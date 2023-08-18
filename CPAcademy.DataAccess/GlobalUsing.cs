cffbf012b08da524f29ce981ea8081b598f0baa7
Moamen Mostafa <mmoamen270@gmail.com>
2023-08-18 15:06:47 +0300

    Rename Global to GlobalUsing

--- CPAcademy.DataAccess/GlobalUsing.cs
+++ CPAcademy.DataAccess/GlobalUsing.cs
@@ -2,3 +2,5 @@ global using CPAcademy.Models;
 global using Microsoft.EntityFrameworkCore;
 global using CPAcademy.DataAccess.Repository.IRepository;
 global using System.Linq.Expressions;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
