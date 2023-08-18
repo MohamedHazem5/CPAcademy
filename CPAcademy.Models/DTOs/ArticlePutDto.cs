12148366986cdba542ca87517615ea897aee4cbf
Moamen Mostafa <mmoamen270@gmail.com>
2023-08-18 23:15:35 +0300

    Article Dtos

---
+++ CPAcademy.Models/DTOs/ArticlePutDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CPAcademy.Models.DTOs
+{
+    public class ArticlePutDto
+    {
+        public int Id { get; set; }
+        public int LectureId { get; set; }
+        public string Content { get; set; }
+    }
+}
