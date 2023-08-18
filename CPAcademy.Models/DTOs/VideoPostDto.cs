0a64b2307da9dfcf51c2881c6026fd33a33cb164
Moamen Mostafa <mmoamen270@gmail.com>
2023-08-18 23:16:01 +0300

    Video Dtos

---
+++ CPAcademy.Models/DTOs/VideoPostDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CPAcademy.Models.DTOs
+{
+    public class VideoPostDto
+    {
+        public string Url { get; set; }
+        public int LectureId { get; set; }
+    }
+}
