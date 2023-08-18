b8bbc4585873db9350c9e75dc931d034718d4463
Moamen Mostafa <mmoamen270@gmail.com>
2023-08-18 23:15:02 +0300

    Modify Lecture Controller

--- CPAcademy/Controllers/LectureController.cs
+++ CPAcademy/Controllers/LectureController.cs
@@ -11,7 +11,7 @@ namespace CPAcademy.Controllers
             _unitOfWork = unitOfWork;
         }
         [HttpGet("{id}")]
-        public async Task<IActionResult> index(int id)
+        public async Task<IActionResult> Index(int id)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
@@ -21,7 +21,7 @@ namespace CPAcademy.Controllers
             return Ok(Lecture);
         }
         [HttpGet("BySection/{id}")]
-        public async Task<IActionResult> ByCourse(int id)
+        public async Task<IActionResult> BySection(int id)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
@@ -31,6 +31,42 @@ namespace CPAcademy.Controllers
                 return NotFound();
 
             return Ok(Sections);
+        }
+        [HttpPost("Add")]
+        public async Task<IActionResult> AddLecture(LecturePostDto lectureDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new { state = ModelState, lecture = lectureDto });
+            var lecture = _mapper.Map<Lecture>(lectureDto);
+            lecture.Time = TimeSpan.FromSeconds(lectureDto.Time); // int to TimeSpan 
+            await _unitOfWork.Lecture.AddAsync(lecture);
+            await _unitOfWork.Save();
+            return Ok(lecture);
+        }
+
+        [HttpPut("Edit")]
+        public async Task<IActionResult> EditLecture(LecturePutDto lectureDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new { modelState = ModelState, Lecture = lectureDto });
+            var lecture = _mapper.Map<Lecture>(lectureDto);
+            lecture.Time = TimeSpan.FromSeconds(lectureDto.Time); // int to TimeSpan 
+            _unitOfWork.Lecture.Update(lecture);
+            await _unitOfWork.Save();
+            return Ok(lecture);
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> DeleteLecture(int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var lecture = await _unitOfWork.Lecture.GetFirstOrDefaultAsync(c => c.Id == id);
+            if (lecture == null)
+                return NotFound();
+            _unitOfWork.Lecture.Delete(lecture);
+            await _unitOfWork.Save();
+            return Ok(lecture);
         }
     }
 }
