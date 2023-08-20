8ccd5079ca82eca3b32838a8686536015b890fca
Mohamed Hazem <100438594+MohamedHazem5@users.noreply.github.com>
2023-08-20 19:41:19 +0300

    Complete AccountController

---
+++ CPAcademy.Models/DTOs/ChangePasswordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CPAcademy.Models.DTOs
+{
+    public class ChangePasswordDto
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string Email { get; set; }
+    }
+}
