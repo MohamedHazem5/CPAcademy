39f218bde0bf262e0d1dedee318f141524e7b7ea
Moamen Mostafa <mmoamen270@gmail.com>
2023-09-07 23:56:14 +0200

    Add Edit Profile & get data by token Edit Login & register

--- CPAcademy.Models/DTOs/UserEditDto.cs
+++ CPAcademy.Models/DTOs/UserEditDto.cs
@@ -1,13 +1,17 @@
 ﻿using CPAcademy.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CPAcademy.Models.DTOs
 {
-    public class UserDto
+    public class UserEditDto
     {
         public string Email { get; set; }
         public Gender Gender { get; set; }
         public string ImgURL { get; set; }
-        public string Token { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
